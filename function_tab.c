fcc01983cad9002482fa1d4f196e84117926366b
KillianXR <9756@holbertonstudents.com>
2024-11-26 17:10:54 +0100

    function_tab.c

---
+++ function_tab.c
@@ -0,0 +1,21 @@
+int _putchar(char)
+{
+	return (write(1, &c, 1));
+}
+
+int _putint(int)
+{
+	return (write(1, &i, 1))
+}
+
+int _putfloat(float)
+{
+	return (write(1, &f, 1))
+}
+
+void _putstr(char *str)
+{
+	while *str != '\0'
+	(write (1, &c, 1))
+	str++
+}
